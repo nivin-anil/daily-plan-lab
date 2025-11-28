496d40580ac953204ad32988d0d9d28c63504025
NivinAnil <nivin@kodnest.com>
2025-11-28 14:03:52 +0530

    lab 4 setup-2

--- DailyPlan.java
+++ DailyPlan.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class P1Lab4 {
+public class DailyPlan {
 
     public static void main(String[] args) {
         System.out.println("=======================================");
